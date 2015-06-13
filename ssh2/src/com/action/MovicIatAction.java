44868856975981ff656b7b4032d64f7213ae532d
li hao <mrhopelee@gmail.com>
2015-06-13 12:57:11 +0800

    给自己的代码加注释

--- ssh2/src/com/action/MovicIatAction.java
+++ ssh2/src/com/action/MovicIatAction.java
@@ -24,6 +24,12 @@ public class MovicIatAction {
 	private HttpServletRequest request = null;
 	private HttpSession hs = null;
 
+	/*
+	 * 分页 getMovicPaging()方法 根据whereSql执行查询，得到结果总数count
+	 * 用结果总数count和setPageSize(int i)方法进行分页， 得到： 一共有paging.rowCount 部电影 每页
+	 * paging.pageSize部 分有paging.pageCount 页 并初始化paging.pageNow(当前页)在第1 页（或第0页）
+	 * 最后把paging对象存入httpsession
+	 */
 	public Paging getMovicPaging(String whereSql) {
 		int count = movicIatServer.getMovicPaging(whereSql);
 		if (paging == null) {
@@ -31,7 +37,7 @@ public class MovicIatAction {
 		}
 
 		paging.setRowCount(count);
-		paging.setPageSize(5);// 设置一页有多少条记录
+		paging.setPageSize(5);// 设置每页 paging.pageSize部 电影
 		paging.sumPageCount();
 		paging.checkPageNow();
 
@@ -43,10 +49,8 @@ public class MovicIatAction {
 	}
 
 	/*
-	 * public String moviciatAllSetSession() { String whereSql =
-	 * "SELECT mi.movicOid, mi.movicName, mi.movicImdbScore, mi.movicPlayDate, mi.movicPost "
-	 * + "FROM MovicInfo as mi ORDER BY mi.movicPlayDate desc"; return
-	 * moviciatSetSession(whereSql); }
+	 * 设置地区与类型 moviciatsetAT()方法 获取存入httpsession的movicarea对象和movictype对象
+	 * 根据用户点击的连接设置对应的类型或地区
 	 */
 	public String moviciatsetAT() {
 		request = ServletActionContext.getRequest();
@@ -79,14 +83,15 @@ public class MovicIatAction {
 			hs.removeAttribute("mtstr");
 			hs.setAttribute("mtstr", movictype.getType());
 		}
-		/*
-		 * System.out.println(hs.getAttribute("mastr").toString() + " and " +
-		 * hs.getAttribute("mtstr").toString());
-		 */
 
 		return "success";
 	}
 
+	/*
+	 * 设置whereSql moviciatMixSetSession()方法
+	 * 获取存入httpsession的movicarea对象和movictype对象
+	 * 动态改变whereSql语句，从而得到想要的查询结果(xx地区xx类型的电影) 后面还要增加按时间或按评分排序
+	 */
 	public String moviciatMixSetSession() {
 		request = ServletActionContext.getRequest();
 		hs = request.getSession(true);
@@ -122,12 +127,16 @@ public class MovicIatAction {
 
 	}
 
+	/*
+	 * 分页，查询 moviciatSetSession() moviciatMixSetSession()方法传入whereSql语句
+	 * 分页：getMovicPaging()方法得到paging 查询：根据paging与whereSql执行查询
+	 */
 	public String moviciatSetSession(String whereSql) {
 		Paging newpaging = getMovicPaging(whereSql);
 		List<MovicInfo> mi;
 		if (newpaging.getPageCount() > 0) {
-			mi = (List<MovicInfo>) movicIatServer
-					.searchMovicInfoService(whereSql, newpaging);
+			mi = (List<MovicInfo>) movicIatServer.searchMovicInfoService(
+					whereSql, newpaging);
 		} else {
 			mi = null;
 		}
